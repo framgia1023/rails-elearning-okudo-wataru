e5e03906466474a5e5a3feb7541ebea761f5b0b9
okodoon <31516823+okodoon@users.noreply.github.com>
2018-10-09 17:13:16 +0800

    remake avator (#10)
    * remake avator
    
    * fix avator function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_075214) do
+ActiveRecord::Schema.define(version: 2018_10_09_082327) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "follower_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_075214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "image"
   end
 
 end

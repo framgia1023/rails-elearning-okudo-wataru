8f545d017291e2c7ea88686cac0a8f79e7a43772
okodoon <31516823+okodoon@users.noreply.github.com>
2018-10-10 16:23:39 +0800

    user category index and show (#12)
    * user category index and show
    
    * add error message to create and edit category
    
    * add pagenate to user category index and admin category index
    
    * make column in user category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,11 @@
 class CategoriesController < ApplicationController
 
+	def index
+		@categories = Category.all.paginate(page: params[:page], per_page: 9)
+	end
+
+	def show
+		@category = Category.find(params[:id])
+	end
+
 end

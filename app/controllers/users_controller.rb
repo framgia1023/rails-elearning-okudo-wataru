1cd5bd90f883c9086e9bee6f802c18efcb83c979
Yuichi Okodo <okdokd1031@gmail.com>
2018-10-16 15:58:27 +0800

    follow comment and delete all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 	def index
 		# @users = User.all
-		@users = User.all.paginate(page: params[:page], per_page: 6)
+		@users = User.paginate(page: params[:page], per_page: 6)
 	end
 
 	def show
